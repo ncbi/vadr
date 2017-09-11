056f0b55ffdcc0b5aa8baa35004782a608088746
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2017-09-11 10:25:47 -0400

    Version number bumped to 0.16

--- dnaorg_test_origin_s2.pl
+++ dnaorg_test_origin_s2.pl
@@ -98,8 +98,8 @@ my $options_okay =
 my $total_seconds = -1 * secondsSinceEpoch(); # by multiplying by -1, we can just add another secondsSinceEpoch call at end to get total time
 my $executable    = $0;
 my $date          = scalar localtime();
-my $version       = "0.11";
-my $releasedate   = "July 2016";
+my $version       = "0.16";
+my $releasedate   = "Sept 2016";
 
 # print help and exit if necessary
 if((! $options_okay) || ($GetOptions_H{"-h"})) { 
