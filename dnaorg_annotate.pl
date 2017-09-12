7c11de34534ec4a706d3260fe0f53fbcbcf484e9
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2017-09-12 14:31:38 -0400

    Bumps version to 0.17

--- dnaorg_annotate.pl
+++ dnaorg_annotate.pl
@@ -338,7 +338,7 @@ my $options_okay =
 my $total_seconds = -1 * secondsSinceEpoch(); # by multiplying by -1, we can just add another secondsSinceEpoch call at end to get total time
 my $executable    = $0;
 my $date          = scalar localtime();
-my $version       = "0.16";
+my $version       = "0.17";
 my $releasedate   = "Sept 2017";
 
 # make *STDOUT file handle 'hot' so it automatically flushes whenever we print to it
