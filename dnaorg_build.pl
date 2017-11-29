3d6f297a1a359e0ea2583a14f3290bffc6d06041
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2017-11-29 06:53:01 -0500

    Bumps version to 0.21

--- dnaorg_build.pl
+++ dnaorg_build.pl
@@ -159,7 +159,7 @@ my $options_okay =
 my $total_seconds = -1 * secondsSinceEpoch(); # by multiplying by -1, we can just add another secondsSinceEpoch call at end to get total time
 my $executable    = $0;
 my $date          = scalar localtime();
-my $version       = "0.20";
+my $version       = "0.21";
 my $releasedate   = "Nov 2017";
 
 # print help and exit if necessary
