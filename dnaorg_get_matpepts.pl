32bdabe7e637bd91f13b3ac6d87e90a1691c81a3
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2018-02-13 10:57:08 -0500

    Bumps version to 0.26 in scripts

--- dnaorg_get_matpepts.pl
+++ dnaorg_get_matpepts.pl
@@ -97,8 +97,8 @@ my $options_okay =
 my $total_seconds = -1 * secondsSinceEpoch(); # by multiplying by -1, we can just add another secondsSinceEpoch call at end to get total time
 my $executable    = $0;
 my $date          = scalar localtime();
-my $version       = "0.25";
-my $releasedate   = "Jan 2018";
+my $version       = "0.26";
+my $releasedate   = "Feb 2018";
 
 # print help and exit if necessary
 if((! $options_okay) || ($GetOptions_H{"-h"})) { 
