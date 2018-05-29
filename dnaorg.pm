6c2a136fac472346ce587bd72c8acdfe5f22e60a
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2018-05-29 12:47:13 -0400

    Makes --cyk and --acyk default'

--- dnaorg.pm
+++ dnaorg.pm
@@ -7770,10 +7770,10 @@ sub runCmscanOrNhmmscan {
 #      $opts .= " --max -E 0.01 "; # with --max, a lot more FPs get through the filter, so we enforce an E-value cutoff
     }
     elsif($do_mid) { 
-      $opts .= " --mid -E 0.1 --noF6 --onepass --tau 0.001 "; # with --mid, more FPs get through the filter, so we enforce an E-value cutoff
+      $opts .= " --mid -E 0.1 --noF6 --onepass --tau 0.001 --cyk --acyk "; # with --mid, more FPs get through the filter, so we enforce an E-value cutoff
     }
     else { 
-      $opts .= " --F1 0.02 --F2 0.001 --F2b 0.001 --F3 0.00001 --F3b 0.00001 --F4 0.0002 --F4b 0.0002 --F5 0.0002 --noF6 --onepass --tau 0.001 ";
+      $opts .= " --F1 0.02 --F2 0.001 --F2b 0.001 --F3 0.00001 --F3b 0.00001 --F4 0.0002 --F4b 0.0002 --F5 0.0002 --noF6 --onepass --tau 0.001 --cyk --acyk ";
     }
     # finally add --nohmmonly if we're not a big model
     if(! $do_big) { # do not use hmm unless model is big
