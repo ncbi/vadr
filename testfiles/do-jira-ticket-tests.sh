fe432678e6f8c3927f1c31caf8c6d6634dac1427
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2018-09-12 07:01:25 -0400

    Simplifies jira ticket test, updates expected output

--- testfiles/do-jira-ticket-tests.sh
+++ testfiles/do-jira-ticket-tests.sh
@@ -1,34 +1,12 @@
 BUILDDIR=/panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/dnaorg-build-directories
 
-# viv-664
 # https://jira.ncbi.nlm.nih.gov/browse/VIV-664
-#$DNAORGDIR/dnaorg_scripts/dnaorg_test.pl --dirbuild $BUILDDIR/norovirus-builds -f $DNAORGDIR/dnaorg_scripts/testfiles/jira-viv-664.testin dt-jira-viv-664
-
-# viv-665
 # https://jira.ncbi.nlm.nih.gov/browse/VIV-665
-#$DNAORGDIR/dnaorg_scripts/dnaorg_test.pl --dirbuild $BUILDDIR/norovirus-builds -f $DNAORGDIR/dnaorg_scripts/testfiles/jira-viv-665.testin dt-jira-viv-665
-
-# viv-697
 # https://jira.ncbi.nlm.nih.gov/browse/VIV-697
-#$DNAORGDIR/dnaorg_scripts/dnaorg_test.pl --dirbuild $BUILDDIR/norovirus-builds -f $DNAORGDIR/dnaorg_scripts/testfiles/jira-viv-697.testin dt-jira-viv-697
-
-# viv-698
 # https://jira.ncbi.nlm.nih.gov/browse/VIV-698
-#$DNAORGDIR/dnaorg_scripts/dnaorg_test.pl --dirbuild $BUILDDIR/norovirus-builds -f $DNAORGDIR/dnaorg_scripts/testfiles/jira-viv-698.testin dt-jira-viv-698
-
-# viv-789
 # https://jira.ncbi.nlm.nih.gov/browse/VIV-789
-#$DNAORGDIR/dnaorg_scripts/dnaorg_test.pl --dirbuild $BUILDDIR/norovirus-builds -f $DNAORGDIR/dnaorg_scripts/testfiles/jira-viv-789.testin dt-jira-viv-789
-
-# viv-792
 # https://jira.ncbi.nlm.nih.gov/browse/VIV-792
-#$DNAORGDIR/dnaorg_scripts/dnaorg_test.pl --dirbuild $BUILDDIR/norovirus-builds -f $DNAORGDIR/dnaorg_scripts/testfiles/jira-viv-792.testin dt-jira-viv-792
-
-# viv-793
 # https://jira.ncbi.nlm.nih.gov/browse/VIV-793
-#$DNAORGDIR/dnaorg_scripts/dnaorg_test.pl --dirbuild $BUILDDIR/norovirus-builds -f $DNAORGDIR/dnaorg_scripts/testfiles/jira-viv-793.testin dt-jira-viv-793
-
-# viv-818
 # https://jira.ncbi.nlm.nih.gov/browse/VIV-818
-$DNAORGDIR/dnaorg_scripts/dnaorg_test.pl --dirbuild $BUILDDIR/norovirus-builds -f $DNAORGDIR/dnaorg_scripts/testfiles/jira-viv-818.testin dt-jira-viv-818
+$DNAORGDIR/dnaorg_scripts/dnaorg_test.pl --dirbuild $BUILDDIR/norovirus-builds -f $DNAORGDIR/dnaorg_scripts/testfiles/jira-viv.testin dt-jira-viv
 
