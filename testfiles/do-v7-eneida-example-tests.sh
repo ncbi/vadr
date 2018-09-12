644a9b9969b432c6f8059d124d7a6c9fb1fb98de
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2018-09-12 06:21:05 -0400

    Adds current v7 test files, renames previous ones with eneida prefix

--- testfiles/do-v7-eneida-example-tests.sh
+++ testfiles/do-v7-eneida-example-tests.sh
@@ -1,5 +1,9 @@
 BUILDDIR=/panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/dnaorg-build-directories
 
+# same as do-v7-current-example-tests.sh 
+# except compares output of v7 tests with Eneida's expected output instead of 
+# 'current' expected output from most recent stable release
+
 # v7 norovirus:
 # https://confluence.ncbi.nlm.nih.gov/pages/viewpage.action?spaceKey=VG&title=Annotation+Test+Sets+and+5-Column+Feature+Table
 # original file with expected sequence table:
@@ -8,7 +12,7 @@ BUILDDIR=/panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/dnaorg-b
 # Then to get noro.v7.nocdsgene.nocodonstart.sqtable, I removed all codon_start qualifiers and all gene qualifiers in CDS features, which 
 # dnaorg pipeline does not attempt to annotate.
 # See /panfs/pan1/infernal/notebook/18_0507_dnaorg_virus_example_feature_table_update/00LOG.txt for details
-$DNAORGDIR/dnaorg_scripts/dnaorg_test.pl --skipmsg --dirbuild $BUILDDIR/norovirus-builds/NC_029646 -f $DNAORGDIR/dnaorg_scripts/testfiles/noro.v7.nocdsgene.nocodonstart.testin dt-noro-v7-test
+$DNAORGDIR/dnaorg_scripts/dnaorg_test.pl --skipmsg --dirbuild $BUILDDIR/norovirus-builds/NC_029646 -f $DNAORGDIR/dnaorg_scripts/testfiles/noro.eneida.v7.nocdsgene.nocodonstart.testin dt-eneida-noro-v7-test
 
 # v7 ebolavirus:
 # https://confluence.ncbi.nlm.nih.gov/pages/viewpage.action?spaceKey=VG&title=Annotation+Test+Sets+and+5-Column+Feature+Table
@@ -19,5 +23,5 @@ $DNAORGDIR/dnaorg_scripts/dnaorg_test.pl --skipmsg --dirbuild $BUILDDIR/noroviru
 # all gene qualifiers in CDS features, which 
 # dnaorg pipeline does not attempt to annotate.
 # See /panfs/pan1/infernal/notebook/18_0507_dnaorg_virus_example_feature_table_update/00LOG.txt for details
-$DNAORGDIR/dnaorg_scripts/dnaorg_test.pl --skipmsg --dirbuild $BUILDDIR/ebolavirus-builds/NC_002549 -f $DNAORGDIR/dnaorg_scripts/testfiles/ebola.v7.noutr.nocdsgene.nocodonstart.testin dt-ebola-v7-test
+$DNAORGDIR/dnaorg_scripts/dnaorg_test.pl --skipmsg --dirbuild $BUILDDIR/ebolavirus-builds/NC_002549 -f $DNAORGDIR/dnaorg_scripts/testfiles/ebola.eneida.v7.noutr.nocdsgene.nocodonstart.testin dt-eneida-ebola-v7-test
 
