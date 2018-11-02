c29917b98618dc1424bb0ed46a91cd24da6fdba2
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2018-11-02 16:56:04 -0400

    Fixes dir in test do*sh

--- testfiles/do-r100-r1000-tests.sh
+++ testfiles/do-r100-r1000-tests.sh
@@ -1,6 +1,6 @@
 #BUILDDIR=/panfs/pan1/infernal/notebook/18_0722_virus_duplicate_features/dnaorg-build-directories
-BUILDDIR=/panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/dnaorg-build-directories
-#BUILDDIR=/panfs/pan1/dnaorg/virseqannot/dnaorg-build-directories
+#BUILDDIR=/panfs/pan1/infernal/notebook/18_1002_virus_dnaorg_protein_validation/dnaorg-build-directories
+BUILDDIR=/panfs/pan1/dnaorg/virseqannot/dnaorg-build-directories
 
 # ebola r100
 $DNAORGDIR/dnaorg_scripts/dnaorg_test.pl --dirbuild $BUILDDIR/ebolavirus-builds -f $DNAORGDIR/dnaorg_scripts/testfiles/testin.ebola.r100 e100
