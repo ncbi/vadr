0b34008a0fc3e96375b4b445cc383dee64200be7
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2018-11-20 14:03:26 -0500

    Passes --local onto dnaorg_annotate.pl automatically

--- dnaorg_classify.pl
+++ dnaorg_classify.pl
@@ -541,6 +541,7 @@ else {
     if(opt_IsUsed("--xalntol",    \%opt_HH)) { $auto_add_opts .= ",--xalntol";    }
     if(opt_IsUsed("--xindeltol",  \%opt_HH)) { $auto_add_opts .= ",--xindeltol";  }
     if(opt_IsUsed("--xlonescore", \%opt_HH)) { $auto_add_opts .= ",--xlonescore"; }
+    if(opt_IsUsed("--local",      \%opt_HH)) { $auto_add_opts .= ",--local"; }
     $annotate_non_cons_opts = parseNonConsOptsFile(opt_Get("--optsA", \%opt_HH), "--optsA", $auto_add_opts, $failure_str, $ofile_info_HH{"FH"});
     outputProgressComplete($start_secs, undef, $log_FH, *STDOUT);
   }
@@ -1276,6 +1277,7 @@ else {
             if(opt_IsUsed("--xalntol",    \%opt_HH)) { $annotate_cmd .= sprintf(" --xalntol    %s", opt_Get("--xalntol",    \%opt_HH)); }
             if(opt_IsUsed("--xindeltol",  \%opt_HH)) { $annotate_cmd .= sprintf(" --xindeltol  %s", opt_Get("--xindeltol",  \%opt_HH)); }
             if(opt_IsUsed("--xlonescore", \%opt_HH)) { $annotate_cmd .= sprintf(" --xlonescore %s", opt_Get("--xlonescore", \%opt_HH)); }
+            if(opt_IsUsed("--local",      \%opt_HH)) { $annotate_cmd .= " --local"; }
 
             if($infasta_mode) { 
               $annotate_cmd .= " --infasta $sub_fasta_file --refaccn $ref_list_seqname";
