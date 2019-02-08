4127edaa164ecf7a483436774aba0c6c9160b119
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2019-02-08 10:49:56 -0500

    Adds new tests.

--- testfiles/do-install-tests-local.sh
+++ testfiles/do-install-tests-local.sh
@@ -1,6 +1,6 @@
 # noro r10
 $DNAORGDIR/dnaorg_scripts/dnaorg_test.pl --dirbuild $DNAORGBUILDDIR/norovirus-builds -f $DNAORGDIR/dnaorg_scripts/testfiles/noro.r10.local.testin n10-local
 
-# ebola r10
-$DNAORGDIR/dnaorg_scripts/dnaorg_test.pl --dirbuild $DNAORGBUILDDIR/ebolavirus-builds -f $DNAORGDIR/dnaorg_scripts/testfiles/ebola.r4.local.testin e4-local
+# dengue r5
+$DNAORGDIR/dnaorg_scripts/dnaorg_test.pl --dirbuild $DNAORGBUILDDIR/dengue-builds -f $DNAORGDIR/dnaorg_scripts/testfiles/dengue.r5.local.testin d5-local
 
--- testfiles/do-install-tests-parallel.sh
+++ testfiles/do-install-tests-parallel.sh
@@ -1,6 +1,6 @@
 # noro r10
 $DNAORGDIR/dnaorg_scripts/dnaorg_test.pl --dirbuild $DNAORGBUILDDIR/norovirus-builds -f $DNAORGDIR/dnaorg_scripts/testfiles/noro.r10.parallel.testin n10-parallel
 
-# ebola r10
-$DNAORGDIR/dnaorg_scripts/dnaorg_test.pl --dirbuild $DNAORGBUILDDIR/ebolavirus-builds -f $DNAORGDIR/dnaorg_scripts/testfiles/ebola.r4.parallel.testin e4-parallel
+# dengue r5
+$DNAORGDIR/dnaorg_scripts/dnaorg_test.pl --dirbuild $DNAORGBUILDDIR/dengue-builds -f $DNAORGDIR/dnaorg_scripts/testfiles/dengue.r5.parallel.testin d5-parallel
 
