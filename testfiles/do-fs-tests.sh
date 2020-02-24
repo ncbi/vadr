68d8810ff092b999338b0172dd66495f9c33a1f2
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2020-02-24 12:37:39 -0500

    Adds new test files for entoy100a frameshifts

--- testfiles/do-fs-tests.sh
+++ testfiles/do-fs-tests.sh
@@ -1,3 +1,7 @@
+# entoy100a-fs1
+$VADRSCRIPTSDIR/v-test.pl -f $VADRSCRIPTSDIR/testfiles/entoy100a-fs.testin entoy100a-fs
 # noro.fs
-$VADRSCRIPTSDIR/v-test.pl -f $VADRSCRIPTSDIR/testfiles/noro.fs.testin noro.fs
+#$VADRSCRIPTSDIR/v-test.pl -f $VADRSCRIPTSDIR/testfiles/noro.fs.testin noro.fs
+# noro.fs.multisgm
+#$VADRSCRIPTSDIR/v-test.pl -f $VADRSCRIPTSDIR/testfiles/noro.fs.multisgm.testin noro.fs.multisgm
 
