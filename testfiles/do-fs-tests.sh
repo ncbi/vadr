04ad0ea0d051901b4f0740bb4b70858397fd1604
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2020-02-26 11:46:11 -0500

    Adds first entoy100a-rev files

--- testfiles/do-fs-tests.sh
+++ testfiles/do-fs-tests.sh
@@ -1,5 +1,7 @@
-# entoy100a-fs1
+# entoy100a-fs{1,2,3}
 $VADRSCRIPTSDIR/v-test.pl -f $VADRSCRIPTSDIR/testfiles/entoy100a-fs.testin entoy100a-fs
+# entoy100a-rev-fs{1,2,3}
+$VADRSCRIPTSDIR/v-test.pl -f $VADRSCRIPTSDIR/testfiles/entoy100a-rev-fs.testin entoy100a-rev-fs
 # noro.fs
 #$VADRSCRIPTSDIR/v-test.pl -f $VADRSCRIPTSDIR/testfiles/noro.fs.testin noro.fs
 # noro.fs.multisgm
