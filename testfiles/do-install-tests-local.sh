e8512abb9ae32a41d1a4ad00c2501dcb2426c96d
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2020-04-08 12:19:07 -0400

    Adds do-install-tests-local.sh output for teamcity, and temporary noro.fail.testin test that should fail

--- testfiles/do-install-tests-local.sh
+++ testfiles/do-install-tests-local.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
 
-# noro r10
-$VADRSCRIPTSDIR/v-test.pl -f --rmout $VADRSCRIPTSDIR/testfiles/noro.r10.local.testin n10-local
+# we require 0 or 1 args, if 1 arg, it must be 'teamcity'
+do_teamcity=0
+if [ $# != 0 ]; then
+    if [ $# -gt 1 ] || [ $1 != "teamcity" ]; then 
+        echo "Usage:"
+        echo "$0"
+        echo "OR"
+        echo "$0 teamcity"
+        exit 1;
+    fi
+    # if we get here, there's 1 arg and it's 'teamcity'
+    do_teamcity=1;
+fi
 
-# dengue r5
-$VADRSCRIPTSDIR/v-test.pl -f --rmout $VADRSCRIPTSDIR/testfiles/dengue.r5.local.testin d5-local
+for test in \
+    noro.r10.local.testin \
+    dengue.r5.local.testin \
+    noro.fail.testin \
+; do
+    if [ $do_teamcity == 1 ]; then
+        echo "##teamcity[testStarted name=\"$test\" captureStandardOutput='true']"
+    fi
+
+    # actual v-test call
+    $VADRSCRIPTSDIR/v-test.pl -f --rmout $VADRSCRIPTSDIR/testfiles/$test vt-$test
+    ret_val=$?
+
+    if [ $do_teamcity == 1 ]; then 
+        if [ $ret_val != 0 ]; then
+            echo "##teamcity[testFailed name=\"$test\" message=\"v-test.pl failure\"]"
+        fi
+        echo "##teamcity[testFinished name=\"$test\"]"
+    fi
+done
+
 
