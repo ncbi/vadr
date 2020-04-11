b67255b9e02f87298ae628a4f72b8a7925c1114f
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2020-04-11 09:47:30 -0400

    Updating test files for recent option changes; adds seed tests

--- testfiles/do-seed-tests.sh
+++ testfiles/do-seed-tests.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# noro.r10
-$VADRSCRIPTSDIR/v-test.pl -f --rmout $VADRSCRIPTSDIR/testfiles/entoy100a-fs.testin entoy100a-fs
+# noro r10 seed
+$VADRSCRIPTSDIR/v-test.pl -f --rmout $VADRSCRIPTSDIR/testfiles/noro.r10.seed.testin vt-n10-seed
 
-# noro.r10.1 --overhang 1
-$VADRSCRIPTSDIR/v-test.pl -f --rmout $VADRSCRIPTSDIR/testfiles/entoy100a-rev-fs.testin entoy100a-rev-fs
-
+# dengue r5 seed
+$VADRSCRIPTSDIR/v-test.pl -f --rmout $VADRSCRIPTSDIR/testfiles/dengue.r5.seed.testin vt-d5-seed
