8cf49f95622a12cf598f78c75a7b184686319c1c
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2020-04-20 06:38:43 -0400

    Updates tests, adds dengue.2 models

--- testfiles/do-all-tests.sh
+++ testfiles/do-all-tests.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-sh do-install-tests-local.sh
-sh do-fs-tests.sh
-sh do-replace-tests.sh
-sh do-seed-tests.sh 
-# sh do-install-tests-parallel.sh
-# sh do-r100-tests-local.sh
-# sh do-r1000-tests-parallel.sh
+set -e
+
+sh $VADRSCRIPTSDIR/testfiles/do-install-tests-local.sh
+sh $VADRSCRIPTSDIR/testfiles/do-fs-tests.sh
+sh $VADRSCRIPTSDIR/testfiles/do-replace-tests.sh
+sh $VADRSCRIPTSDIR/testfiles/do-seed-tests.sh
+sh $VADRSCRIPTSDIR/testfiles/do-outaln-tests.sh
+# sh $VADRSCRIPTSDIR/testfiles/do-install-tests-parallel.sh
+# sh $VADRSCRIPTSDIR/testfiles/do-r100-tests-local.sh
+# sh $VADRSCRIPTSDIR/testfiles/do-r1000-tests-parallel.sh
