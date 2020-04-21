b43cec53abb69368d2cb43bafa94af216704bc58
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2020-04-21 07:21:18 -0400

    Updates do-all-tests.sh

--- testfiles/do-all-tests.sh
+++ testfiles/do-all-tests.sh
@@ -6,6 +6,8 @@ sh $VADRSCRIPTSDIR/testfiles/do-fs-tests.sh
 sh $VADRSCRIPTSDIR/testfiles/do-replace-tests.sh
 sh $VADRSCRIPTSDIR/testfiles/do-seed-tests.sh
 sh $VADRSCRIPTSDIR/testfiles/do-outaln-tests.sh
+sh $VADRSCRIPTSDIR/testfiles/do-nindel-tests.sh
+sh $VADRSCRIPTSDIR/testfiles/do-hmmer-tests.sh
+
+# to test if -p will work (requires qsub and qsub flags similar to NCBI internal set-up)
 # sh $VADRSCRIPTSDIR/testfiles/do-install-tests-parallel.sh
-# sh $VADRSCRIPTSDIR/testfiles/do-r100-tests-local.sh
-# sh $VADRSCRIPTSDIR/testfiles/do-r1000-tests-parallel.sh
