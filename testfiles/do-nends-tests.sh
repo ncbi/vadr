0b7a8c566dc0276b247e8950b45c3964eee074f7
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2020-05-19 15:32:53 -0400

    Updates nends tests more, consolidates into one file per fwd and rev

--- testfiles/do-nends-tests.sh
+++ testfiles/do-nends-tests.sh
@@ -3,6 +3,15 @@
 RETVAL=0;
 
 $VADRSCRIPTSDIR/v-test.pl -f --rmout $VADRSCRIPTSDIR/testfiles/entoy100a.nends.testin vt-entoy100a-nends
+if [ $? != 0 ]; then
+   RETVAL=1;
+fi   
+
+$VADRSCRIPTSDIR/v-test.pl -f --rmout $VADRSCRIPTSDIR/testfiles/entoy100a-rev.nends.testin vt-entoy100a-rev-nends
+if [ $? != 0 ]; then
+   RETVAL=1;
+fi   
+
 if [ $? == 0 ]; then
    echo "Success: all tests passed"
    exit 0
