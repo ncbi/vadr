d169e3c644ebb7b50bee35b9ad7231a5db502035
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2020-06-17 08:15:54 -0400

    Bumps develop version to 1.1dev1

--- v-annotate.pl
+++ v-annotate.pl
@@ -444,7 +444,7 @@ my $executable    = (defined $execname_opt) ? $execname_opt : "v-annotate.pl";
 my $usage         = "Usage: $executable [-options] <fasta file to annotate> <output directory to create>\n";
 my $synopsis      = "$executable :: classify and annotate sequences using a CM library";
 my $date          = scalar localtime();
-my $version       = "1.1";
+my $version       = "1.1dev1";
 my $releasedate   = "May 2020";
 my $pkgname       = "VADR";
 
