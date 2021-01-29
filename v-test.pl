dc5de1f58329a708a3c53e6f33d126ec6c1db4a3
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2021-01-28 19:18:01 -0500

    Bumps version to 1.1.2-dev1

--- v-test.pl
+++ v-test.pl
@@ -79,8 +79,8 @@ my $executable    = (defined $execname_opt) ? $execname_opt : "v-test.pl";
 my $usage         = "Usage: $executable [-options] <input test file e.g. testfiles/testin.1> <output directory to create>\n";
 my $synopsis      = "$executable :: test VADR scripts [TEST SCRIPT]";
 my $date          = scalar localtime();
-my $version       = "1.1.2";
-my $releasedate   = "Nov 2020";
+my $version       = "1.1.2-dev1";
+my $releasedate   = "Jan 2021";
 my $pkgname       = "VADR";
 
 # print help and exit if necessary
