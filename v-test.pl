796ac47bfe4d3a7d72649dad923d7d9ae5e32ba9
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2021-04-12 12:14:43 -0400

    Changes version from 1.2dev8 to 1.2

--- v-test.pl
+++ v-test.pl
@@ -79,7 +79,7 @@ my $executable    = (defined $execname_opt) ? $execname_opt : "v-test.pl";
 my $usage         = "Usage: $executable [-options] <input test file e.g. testfiles/testin.1> <output directory to create>\n";
 my $synopsis      = "$executable :: test VADR scripts [TEST SCRIPT]";
 my $date          = scalar localtime();
-my $version       = "1.2dev8";
+my $version       = "1.2";
 my $releasedate   = "April 2021";
 my $pkgname       = "VADR";
 
