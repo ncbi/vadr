4bacc3b3b76f0e86343f7f5dd51341eb2a583f61
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2021-07-14 17:29:23 -0400

    Tweaking alerts.md documentation wording

--- documentation/alert-files/example-insert.sh
+++ documentation/alert-files/example-insert.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 perl $VADRSCRIPTSDIR/v-annotate.pl --minpvlen 3 --nmaxins 2 --pv_skip --keep --mkey toy50 -mdir $VADRSCRIPTSDIR/documentation/alert-files -f $VADRSCRIPTSDIR/documentation/alert-files/example-insert.fa va-example-insert
