8d83fa936a14aba3ec9921dbf32eeba066cede53
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2021-07-14 21:44:49 -0400

    Adds bin/bash to beginning of each example script

--- documentation/alert-files/example-indefend.sh
+++ documentation/alert-files/example-indefend.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 perl $VADRSCRIPTSDIR/v-annotate.pl --minpvlen 3 --pv_skip --keep --mkey toy50 -mdir $VADRSCRIPTSDIR/documentation/alert-files -f $VADRSCRIPTSDIR/documentation/alert-files/example-indefend.fa va-example-indefend
--- documentation/alert-files/example-indfstrn.sh
+++ documentation/alert-files/example-indfstrn.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 perl $VADRSCRIPTSDIR/v-annotate.pl --indefstr 12 --minpvlen 3 --pv_skip --keep --mkey toy50 -mdir $VADRSCRIPTSDIR/documentation/alert-files -f $VADRSCRIPTSDIR/documentation/alert-files/example-indfstrn.fa va-example-indfstrn
