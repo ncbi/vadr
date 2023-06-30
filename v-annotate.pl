069029ffc6bceb5d2c5b136bd2296fac4ac52f8a
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2023-06-30 11:52:54 -0400

    Reinstates misc_feature output, incorrectly removed with fix to iss72

--- v-annotate.pl
+++ v-annotate.pl
@@ -10515,7 +10515,7 @@ sub output_feature_table {
             } # end of 'if($is_cds)' entered to determine codon_start
             
             # convert coordinate string to output string
-            my $out_feature_type = vdr_FeatureOutType($ftr_info_AHR, $ftr_idx);
+            my $out_feature_type = ($feature_type eq "misc_feature") ? "misc_feature" : vdr_FeatureOutType($ftr_info_AHR, $ftr_idx);
             $ftr_out_str = helper_ftable_coords_to_out_str($ftr_ftbl_coords_str, $out_feature_type, $FH_HR);
             
             # add qualifiers: product, gene, exception and codon_start
