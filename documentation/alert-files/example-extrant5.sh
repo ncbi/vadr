4961d7c1b2a541e38b1b15c9b4cacf359bc167ed
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2023-07-11 12:08:00 -0400

    Adds extrant5/3 examples to alerts.md

---
+++ documentation/alert-files/example-extrant5.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+perl $VADRSCRIPTSDIR/v-annotate.pl --minpvlen 3 --lowcov 0.8 --pv_skip --keep -m $VADRSCRIPTSDIR/documentation/alert-files/toy50.vadrprior.cm -i $VADRSCRIPTSDIR/documentation/alert-files/toy50.minfo -f $VADRSCRIPTSDIR/documentation/alert-files/example-extrant5.fa va-example-extrant5
