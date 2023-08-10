c9ecc13db9f598c3126925d5ba90f85b737c1909
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2023-08-10 14:41:26 -0400

    Adds reverse complement ss tests

--- testfiles/do-ss-tests.sh
+++ testfiles/do-ss-tests.sh
@@ -9,7 +9,7 @@ if [ "$?" -ne 0 ]; then
 fi   
 
 # entoy200b-rev-ss
-#$VADRSCRIPTSDIR/v-test.pl -f --rmout $VADRSCRIPTSDIR/testfiles/entoy200b-rev-ss.testin entoy200b-rev-ss
-#if [ "$?" -ne 0 ]; then
-#   RETVAL=1;
-#fi   
+$VADRSCRIPTSDIR/v-test.pl -f --rmout $VADRSCRIPTSDIR/testfiles/entoy200b-rev-ss.testin entoy200b-rev-ss
+if [ "$?" -ne 0 ]; then
+   RETVAL=1;
+fi   
