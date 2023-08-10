3c6a612c54ead5093a89785ac7507080d4e2c491
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2023-08-10 14:12:42 -0400

    Modifies entoy200b intron slightly

--- testfiles/do-ss-tests.sh
+++ testfiles/do-ss-tests.sh
@@ -2,7 +2,14 @@
 
 RETVAL=0;
 
+# entoy200b-ss
 $VADRSCRIPTSDIR/v-test.pl -f --rmout $VADRSCRIPTSDIR/testfiles/entoy200b-ss.testin entoy200b-ss
 if [ "$?" -ne 0 ]; then
    RETVAL=1;
 fi   
+
+# entoy200b-rev-ss
+#$VADRSCRIPTSDIR/v-test.pl -f --rmout $VADRSCRIPTSDIR/testfiles/entoy200b-rev-ss.testin entoy200b-rev-ss
+#if [ "$?" -ne 0 ]; then
+#   RETVAL=1;
+#fi   
