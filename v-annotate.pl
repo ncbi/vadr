1d7450fe44e66a348a63d68cd29652daec3a688d
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2023-09-18 11:39:47 -0400

    Bumps version to 1.6dev

--- v-annotate.pl
+++ v-annotate.pl
@@ -629,8 +629,8 @@ my $executable    = (defined $execname_opt) ? $execname_opt : "v-annotate.pl";
 my $usage         = "Usage: $executable [-options] <fasta file to annotate> <output directory to create>\n";
 my $synopsis      = "$executable :: classify and annotate sequences using a model library";
 my $date          = scalar localtime();
-my $version       = "1.5.1";
-my $releasedate   = "Feb 2023";
+my $version       = "1.6dev";
+my $releasedate   = "Sep 2023";
 my $pkgname       = "VADR";
 
 # make *STDOUT file handle 'hot' so it automatically flushes whenever we print to it
