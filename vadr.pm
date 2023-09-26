363ff6e172b90273c6f23cc8f9424d381d64ec8d
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2023-09-26 11:45:11 -0400

    Updates cmalign error detection for infernal 1.1.5 syntax

--- vadr.pm
+++ vadr.pm
@@ -5900,7 +5900,7 @@ sub vdr_CmalignCheckStdOutput {
     # job did NOT finish successfully, check for mx overflow error
     my $error_line = `grep ^Error $stdout_file | tail -n 1`;
     if($error_line =~ m/\r$/) { chop $error_line; } # remove ^M if it exists
-    if($error_line =~ /Error: .+ alignment mxes need (\d+\.\d+)/) { 
+    if($error_line =~ /Error: .+ mxes need (\d+\.\d+)/) { 
       if(defined $ret_mxsize_R) { 
         $$ret_mxsize_R = $1;
       }
