6796451c1ec02d0946ab44e28d5d483f3862ad96
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2023-11-02 12:08:58 -0400

    Adds success/failure output to do-ss-tests.sh

--- testfiles/do-ss-tests.sh
+++ testfiles/do-ss-tests.sh
@@ -25,3 +25,11 @@ $VADRSCRIPTSDIR/v-test.pl -f --rmout $VADRSCRIPTSDIR/testfiles/entoy200b-rev-ss-
 if [ "$?" -ne 0 ]; then
    RETVAL=1;
 fi   
+
+if [ "$RETVAL" -eq 0 ]; then
+   echo "Success: all tests passed [do-ss-tests.sh]"
+   exit 0
+else 
+   echo "FAIL: at least one test failed [do-ss-tests.sh]"
+   exit 1
+fi
