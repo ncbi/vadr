836df4a4367511f42a5740973c93f6f629bf7536
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2023-11-29 14:36:22 -0500

    Bumps version

--- v-test.pl
+++ v-test.pl
@@ -79,7 +79,7 @@ my $executable    = (defined $execname_opt) ? $execname_opt : "v-test.pl";
 my $usage         = "Usage: $executable [-options] <input test file e.g. testfiles/testin.1> <output directory to create>\n";
 my $synopsis      = "$executable :: test VADR scripts [TEST SCRIPT]";
 my $date          = scalar localtime();
-my $version       = "1.6";
+my $version       = "1.6.1";
 my $releasedate   = "Nov 2023";
 my $pkgname       = "VADR";
 
