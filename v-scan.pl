4ed86692174ec3c03bd45a8feeebcc4852d837c1
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2025-02-10 14:39:55 -0500

    Outputs lib file

--- v-scan.pl
+++ v-scan.pl
@@ -41,9 +41,12 @@ require "sqp_utils.pm";
 
 # make sure required environment variables are set
 my $env_vadr_scripts_dir  = utl_DirEnvVarValid("VADRSCRIPTSDIR");
+my $env_vadr_easel_dir    = utl_DirEnvVarValid("VADREASELDIR");
 
 my %execs_H = (); # hash with paths to all required executables
 $execs_H{"v-annotate.pl"} = $env_vadr_scripts_dir  . "/v-annotate.pl";
+$execs_H{"esl-reformat"}  = $env_vadr_easel_dir    . "/esl-reformat";
+
 utl_ExecHValidate(\%execs_H, undef);
 
 #########################################################
@@ -83,6 +86,7 @@ opt_Add("-f",           "boolean", 0,         $g,    undef, undef,      "force d
 opt_Add("-v",           "boolean", 0,         $g,    undef, undef,      "be verbose",                                                                              "be verbose; output commands to stdout as they're run", \%opt_HH, \@opt_order_A);
 opt_Add("--lone",       "boolean", 0,         $g,    undef, undef,      "exit if at least one sequence matches to multiple libraries",                             "exit if at least one sequence matches to multiple libraries", \%opt_HH, \@opt_order_A);
 opt_Add("--first",      "boolean", 0,         $g,    undef,"--lone",    "if a seq matches > 1 model library use first one [df: use best scoring]",                 "if a seq matches > 1 model library use first one [df: use best scoring]", \%opt_HH, \@opt_order_A);
+opt_Add("--origfa",     "boolean", 0,         $g,    undef,   undef,    "do not copy fasta file prior to analysis, use original",                 "do not copy fasta file prior to analysis, use original", \%opt_HH, \@opt_order_A);
 opt_Add("--keep",       "boolean", 0,         $g,    undef, undef,      "leaving intermediate files on disk",                                                      "do not remove intermediate files, keep them all on disk", \%opt_HH, \@opt_order_A);
 
 # This section needs to be kept in sync (manually) with the opt_Add() section above
@@ -94,6 +98,7 @@ my $options_okay =
                 'v'        => \$GetOptions_H{"-v"},
                 'lone'     => \$GetOptions_H{"--lone"}, 
                 'first'    => \$GetOptions_H{"--first"},
+                'origfa'   => \$GetOptions_H{"--origfa"},
                 'keep'     => \$GetOptions_H{"--keep"});
 
 my $total_seconds = -1 * ofile_SecondsSinceEpoch(); # by multiplying by -1, we can just add another secondsSinceEpoch call at end to get total time
@@ -107,7 +112,6 @@ my $releasedate   = "Feb 2025";
 my $pkgname       = "VADR";
 
 # make *STDOUT file handle 'hot' so it automatically flushes whenever we print to it
-# it is printed to
 select *STDOUT;
 $| = 1;
 
@@ -228,6 +232,28 @@ parse_config_file($in_config_file, \@mkey_A, \%mkey_mdir_H, \%mkey_opts_H, \%opt
 
 ofile_OutputProgressComplete($start_secs, undef, $log_FH, *STDOUT);
 
+###########################################
+# Copy and validate the input sequence file
+###########################################
+my $in_fa_file = undef;
+if(opt_Get("--origfa", \%opt_HH)) { 
+  # --origfa: analyze original fasta file, do not copy it
+  $in_fa_file = $orig_in_fa_file;
+  if(-e $in_fa_file . ".ssi") { unlink $in_fa_file . ".ssi"}; # remove SSI file if it exists, it may be out of date
+}
+else { 
+  # default: copy original fasta file and analyze that, but don't just copy it, 
+  # use 'esl-reformat fasta', this was introduced to sidestep some mysterious 
+  # SSI related issues
+  $in_fa_file = $out_root . ".in.fa";
+  utl_RunCommand($execs_H{"esl-reformat"} . " fasta $orig_in_fa_file > $in_fa_file", opt_Get("-v", \%opt_HH), 0, $FH_HR);
+  ofile_AddClosedFileToOutputInfo(\%ofile_info_HH, "cp.in.fasta", $in_fa_file, $do_keep, $do_keep, "copy of input fasta file");
+  push(@to_remove_A, $in_fa_file);
+  push(@to_remove_A, $in_fa_file . ".ssi");
+}
+my $in_sqfile = Bio::Easel::SqFile->new({ fileLocation => $in_fa_file }); # the sequence file object
+
+#################################################
 # foreach model key, run v-annotate.pl --cls_only
 my %out_dir_H = (); # hash of output directories
 my %sqc_H = ();     # hash of sqc files
@@ -235,22 +261,134 @@ my $mkey;
 foreach $mkey (@mkey_A) {
   $out_dir_H{$mkey} = $dir_tail . "/" . $mkey . ".0";
   $sqc_H{$mkey} = $out_dir_H{$mkey} . "/" . $mkey . ".0.vadr.sqc";
-  $cmd = $execs_H{"v-annotate.pl"} . " -f -s --cls_only --mkey $mkey --mdir $mkey_mdir_H{$mkey} $orig_in_fa_file $out_dir_H{$mkey}";
+  $cmd = $execs_H{"v-annotate.pl"} . " -f -s --origfa --cls_only --mkey $mkey --mdir $mkey_mdir_H{$mkey} $in_fa_file $out_dir_H{$mkey}";
   if(! $do_verbose) { $cmd .= " > /dev/null"; }
   my $start_secs = ofile_OutputProgressPrior(sprintf("Scanning for %s sequences ... ", $mkey), $progress_w, $log_FH, *STDOUT);
   utl_RunCommand($cmd, opt_Get("-v", \%opt_HH), 0, $FH_HR);
   ofile_OutputProgressComplete($start_secs, undef, $log_FH, *STDOUT);
 }
 
-# now parse the output to see if any seqs matched to a model
-my %seq_H = ();
-my @seq_A = ();
-my %seq_mkey_H = ();
-my %seq_mdl_H  = ();
-my %seq_sc_H   = ();
+# Parse sqc files to determine which seqs match best to each library, need
+# to look at all sqc files before assigning sequences to a mkey
+# because we may be determining best library based on score
+my %seq_H         = ();   # 'exists' hash, key is sequence name, value is always '1' 
+my @seq_A         = ();   # array of sequence names
+my %seq_mkey_H    = ();   # key is seq name, value is best mkey for this sequence
+my %seq_mdl_H     = ();   # key is seq name, value is best model for this sequence
+my %seq_grp_H     = ();   # key is seq name, value is group of best model for this sequence
+my %seq_subgrp_H  = ();   # key is seq name, value is subgroup of best model for this sequence
+my %seq_sc_H      = ();   # key is seq name, value is score for best model for this sequence
 foreach $mkey (@mkey_A) {
-  parse_sqc_clsonly_file($sqc_H{$mkey}, \%seq_H, \@seq_A, \%seq_mkey_H, \%seq_mdl_H, \%seq_sc_H, \%opt_HH, $FH_HR);
+  parse_sqc_clsonly_file($sqc_H{$mkey}, $mkey, \%seq_H, \@seq_A, \%seq_mkey_H, \%seq_mdl_H, \%seq_grp_H, \%seq_subgrp_H, \%seq_sc_H, \%opt_HH, $FH_HR);
 }
+
+# Fill per-mkey lists of sequences
+my %seqlist_HA = (); # key is mkey, value is array of sequences that match to this mkey
+my $nmkey = 0;       # number of mkey (libraries) we have at least one sequence to rerun v-annotate.pl for
+
+my %mkey_mdl_ct_HH = ();
+my %mkey_mdl_order_HA = ();
+my %grp_H    = (); # key is mdl name, value is group
+my %subgrp_H = (); # key is mdl name, value is subgroup
+foreach my $seqname (@seq_A) {
+  if(defined $seq_mkey_H{$seqname}) {
+    my $mkey   = $seq_mkey_H{$seqname};
+    my $mdl    = $seq_mdl_H{$seqname};
+    my $grp    = $seq_grp_H{$seqname};
+    my $subgrp = $seq_subgrp_H{$seqname};
+    if(! defined $seqlist_HA{$mkey}) {
+      @{$seqlist_HA{$mkey}} = ();
+      %{$mkey_mdl_ct_HH{$mkey}} = ();
+      @{$mkey_mdl_order_HA{$mkey}} = ();
+      $nmkey++;
+    }
+    if(! defined $mkey_mdl_ct_HH{$mkey}{$mdl}) {
+      $mkey_mdl_ct_HH{$mkey}{$mdl} = 0;
+      push(@{$mkey_mdl_order_HA{$mkey}}, $mdl);
+      $grp_H{$mdl}    = $grp;
+      $subgrp_H{$mdl} = $subgrp;
+    }
+    push(@{$seqlist_HA{$mkey}}, $seqname);
+    $mkey_mdl_ct_HH{$mkey}{$mdl}++;
+  }
+} 
+
+if($nmkey > 0) { 
+  foreach $mkey (@mkey_A) {
+    if(defined $seqlist_HA{$mkey}) {
+      my $mkey_fasta_file = $dir_tail . "/" . $mkey . ".fa";
+      my $out_dir = $dir_tail . "/" . $mkey;
+      $in_sqfile->fetch_seqs_given_names(\@{$seqlist_HA{$mkey}}, 60, $mkey_fasta_file);
+      $cmd = $execs_H{"v-annotate.pl"} . " --mkey $mkey --mdir $mkey_mdir_H{$mkey} $mkey_opts_H{$mkey} $mkey_fasta_file $out_dir";
+      if(! $do_verbose) { $cmd .= " > /dev/null"; }
+      my $start_secs = ofile_OutputProgressPrior(sprintf("Annotating %s sequences (%d) ... ", $mkey, scalar(@{$seqlist_HA{$mkey}})), $progress_w, $FH_HR->{"log"}, *STDOUT);
+      utl_RunCommand($cmd, opt_Get("-v", \%opt_HH), 0, $FH_HR);
+      ofile_OutputProgressComplete($start_secs, undef, $log_FH, *STDOUT);
+    }
+  }
+}
+
+$start_secs = ofile_OutputProgressPrior("Generating tabular output", $progress_w, $log_FH, *STDOUT);
+
+# create the @data_lib_AA
+my $mkey_idx = 1;
+my $mdl_idx = 1;
+
+# open files for writing
+ofile_OpenAndAddFileToOutputInfo(\%ofile_info_HH, "lib", $out_root . ".lib", 1, 1, "per-model library tabular summary file");
+my @head_lib_AA = ();
+my @data_lib_AA = ();
+my @clj_lib_A   = ();
+@{$head_lib_AA[0]} = ("",    "",        "",      "",      "",         "num");
+@{$head_lib_AA[1]} = ("idx", "library", "model", "group", "subgroup", "seqs");
+@clj_lib_A         = (1,     1,         1,       1,       1,          0);
+
+foreach $mkey (@mkey_A) {
+  my $mdl_idx = 1;
+  my $mkey_mdl_idx = sprintf("%d.%d", $mkey_idx, $mdl_idx);
+  if(! defined $mkey_mdl_ct_HH{$mkey}) {
+    push(@data_lib_AA, [$mkey_mdl_idx, $mkey, "-", "-", "-", 0 ]);
+  }
+  else {
+    foreach my $mdl (@{$mkey_mdl_order_HA{$mkey}}) {
+      push(@data_lib_AA, [$mkey_mdl_idx, $mkey, $mdl, $grp_H{$mdl}, $subgrp_H{$mdl}, $mkey_mdl_ct_HH{$mkey}{$mdl} ]);
+      $mdl_idx++;
+    }
+  }
+  $mkey_idx++;
+}
+
+ofile_TableHumanOutput(\@data_lib_AA, \@head_lib_AA, \@clj_lib_A, undef, undef, "  ", "-", "#", "#", "", 0, $FH_HR->{"lib"}, undef, $FH_HR);
+ofile_OutputProgressComplete($start_secs, undef, $log_FH, *STDOUT);
+
+############
+# Conclude #
+############
+output_lib_file_and_remove_temp_files($zero_mdl, \@to_remove_A, \%opt_HH, \%ofile_info_HH);
+                                      
+# remove unwanted files, unless --keep
+if(! opt_Get("--keep", \%opt_HH)) { 
+  my @to_actually_remove_A = (); # sanity check: make sure the files we're about to remove actually exist
+  my %to_actually_remove_H = (); # sanity check: to make sure we don't try to delete 
+  foreach my $to_remove_file (@to_remove_A) { 
+    if((defined $to_remove_file) && (-e $to_remove_file) && (! defined $to_actually_remove_H{$to_remove_file})) { 
+      push(@to_actually_remove_A, $to_remove_file); 
+      $to_actually_remove_H{$to_remove_file} = 1; 
+    }
+  }
+  utl_FileRemoveList(\@to_actually_remove_A, "v-scan.pl", \%opt_HH, $FH_HR);
+}
+
+# output lib file to stdout
+my @file_A = ();
+my $line;
+utl_FileLinesToArray($ofile_info_HH{"fullpath"}{"lib"}, 0, \@file_A, $FH_HR);
+foreach $line (@file_A) {
+  print $line . "\n";
+}
+
+$total_seconds += ofile_SecondsSinceEpoch();
+ofile_OutputConclusionAndCloseFilesOk($total_seconds, $dir, \%ofile_info_HH);
 
 #################################################################
 # Subroutine: parse_config_file
@@ -316,14 +454,17 @@ sub parse_config_file {
 # Purpose:    Parse the .sqc file output from v-annotate.pl --cls_only
 #
 # Arguments:
-#  $sqc_file:     name of sqc file to parse
-#  $seq_HR:       REF to hash of sequence names, key is seq name, value is 1, to fill here
-#  $seq_AR:       REF to array of sequence names, to fill here
-#  $seq_mkey_HR:  REF to hash, key is sequence name, value is winning mkey, to fill here
-#  $seq_mdl_HR:   REF to hash, key is sequence name, value is winning model, to fill here
-#  $seq_sc_HR:    REF to hash, key is sequence name, value is winning score, to fill here
-#  $opt_HHR:      REF to 2D hash of option values, see top of sqp_opts.pm for description
-#  $FH_HR:        REF to hash of file handles
+#  $sqc_file:      name of sqc file to parse
+#  $mkey:          REF model key (e.g. flu) that this sqc file pertains to
+#  $seq_HR:        REF to hash of sequence names, key is seq name, value is 1, to fill here
+#  $seq_AR:        REF to array of sequence names, to fill here
+#  $seq_mkey_HR:   REF to hash, key is sequence name, value is winning mkey, to fill here
+#  $seq_mdl_HR:    REF to hash, key is sequence name, value is winning model, to fill here
+#  $seq_grp_HR:    REF to hash, key is sequence name, value is group of winning model, "-" if undef, to fill here
+#  $seq_subgrp_HR: REF to hash, key is sequence name, value is subgroup of winning model, "-" if undef, to fill here
+#  $seq_sc_HR:     REF to hash, key is sequence name, value is winning score, to fill here
+#  $opt_HHR:       REF to 2D hash of option values, see top of sqp_opts.pm for description
+#  $FH_HR:         REF to hash of file handles
 #
 # Returns:  void
 #           
@@ -332,10 +473,10 @@ sub parse_config_file {
 #################################################################
 sub parse_sqc_clsonly_file { 
   my $sub_name = "parse_sqc_clsonly_file"; 
-  my $nargs_exp = 8;
+  my $nargs_exp = 11;
   if(scalar(@_) != $nargs_exp) { die "ERROR $sub_name entered with wrong number of input args"; }
 
-  my ($sqc_file, $seq_HR, $seq_AR, $seq_mkey_HR, $seq_mdl_HR, $seq_sc_HR, $opt_HHR, $FH_HR) = (@_);
+  my ($sqc_file, $mkey, $seq_HR, $seq_AR, $seq_mkey_HR, $seq_mdl_HR, $seq_grp_HR, $seq_subgrp_HR, $seq_sc_HR, $opt_HHR, $FH_HR) = (@_);
 
   my $do_lone  = opt_Get("--lone", $opt_HHR);
   my $do_first = opt_Get("--first", $opt_HHR);
@@ -353,7 +494,8 @@ sub parse_sqc_clsonly_file {
       if(scalar(@el_A) != 21) { 
         ofile_FAIL("ERROR problem parsing sqc file $sqc_H{$mkey}", 1, $FH_HR);
       }
-      my ($seqname, $pf, $mdl, $score) = ($el_A[1], $el_A[3], $el_A[5], $el_A[8]);
+      my ($seqname, $pf, $mdl, $grp, $subgrp, $score) = ($el_A[1], $el_A[3], $el_A[5], $el_A[6], $el_A[7], $el_A[8]);
+
       if(! defined $seq_HR->{$seqname}) {
         push(@seq_A, $seqname);
         $seq_HR->{$seqname} = 1;
@@ -377,14 +519,85 @@ sub parse_sqc_clsonly_file {
           }
         }
         if($keep_flag) {
-          $seq_mkey_HR->{$seqname} = $mkey;
-          $seq_mdl_HR->{$seqname}  = $mdl;
-          $seq_sc_HR->{$seqname}   = $score;
+          $seq_mkey_HR->{$seqname}   = $mkey;
+          $seq_mdl_HR->{$seqname}    = $mdl;
+          $seq_grp_HR->{$seqname}    = $grp;
+          $seq_subgrp_HR->{$seqname} = $subgrp;
+          $seq_sc_HR->{$seqname}     = $score;
         }
       }
     }
   }
   close(SQC);
+
+  return;
+}
+
+#################################################################
+# Subroutine: output_lib_and_remove_temp_files()
+# Incept:     EPN, Mon Feb 10 14:13:25 2025
+#             based on v-annotate.pl:output_mdl_and_alc_files_and_remove_temp_files()
+# Purpose:    Output the lib file and remove all files
+#             if (@{$to_remove_A}) unless --keep. 
+#
+# Arguments:
+#  $to_remove_AR:   ref to array of files to remove
+#  $opt_HHR:        ref to 2D hash of option values, see top of sqp_opts.pm for description
+#  $ofile_info_HHR: ref to 2D hash of output file information, added to here
+#             
+# Returns:  void
+#
+#################################################################
+sub output_lib_file_and_remove_temp_files { 
+  my $sub_name = "output_lib_file_and_remove_temp_files";
+  my $nargs_exp = 3;
+  if(scalar(@_) != $nargs_exp) { die "ERROR $sub_name entered with wrong number of input args"; }
+
+  my ($to_remove_AR, $opt_HHR, $ofile_info_HHR) = (@_);
+
+  # close the two files we may output to stdout and the log
+  close($ofile_info_HHR->{"FH"}{"lib"});
+  
+  my $FH_HR  = $ofile_info_HH{"FH"};
+  
+  my @conclude_A = ();
+  push(@conclude_A, "#");
+  push(@conclude_A, "# Summary of classified sequences:");
+  push(@conclude_A, "#");
+  my @file_A = ();
+  utl_FileLinesToArray($ofile_info_HHR->{"fullpath"}{"mdl"}, 1, \@file_A, $FH_HR);
+  push(@conclude_A, @file_A);
+  push(@conclude_A, "#");
+  if($do_clsonly) {
+    push(@conclude_A, "# Only classification-related alerts detected due to --cls_only.");
+  }
+  elsif($zero_alt) { 
+    push(@conclude_A, "# Zero alerts were reported.");
+  }
+  else { 
+    push(@conclude_A, "# Summary of reported alerts:");
+    push(@conclude_A, "#");
+    my @file_A = ();
+    utl_FileLinesToArray($ofile_info_HHR->{"fullpath"}{"alc"}, 1, \@file_A, $FH_HR);
+    push(@conclude_A, @file_A);
+  }
+  
+  foreach my $line (@conclude_A) { 
+    ofile_OutputString($FH_HR->{"log"}, 1, $line . "\n");
+  }
+  
+  # remove unwanted files, unless --keep
+  if(! opt_Get("--keep", $opt_HHR)) { 
+    my @to_actually_remove_A = (); # sanity check: make sure the files we're about to remove actually exist
+    my %to_actually_remove_H = (); # sanity check: to make sure we don't try to delete 
+    foreach my $to_remove_file (@to_remove_A) { 
+      if((defined $to_remove_file) && (-e $to_remove_file) && (! defined $to_actually_remove_H{$to_remove_file})) { 
+        push(@to_actually_remove_A, $to_remove_file); 
+        $to_actually_remove_H{$to_remove_file} = 1; 
+      }
+    }
+    utl_FileRemoveList(\@to_actually_remove_A, $sub_name, $opt_HHR, $FH_HR);
+  }
 
   return;
 }
