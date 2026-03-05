46342cc780b44ae1300dcab573eea807008e424f
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2026-03-05 16:07:07 -0500

    Implement diverse-50 metadata filtering logic in v-profprep.pl

---
+++ v-profprep.pl
@@ -0,0 +1,284 @@
+#!/usr/bin/env perl
+#
+# v-profprep.pl
+# Prepare a representative Stockholm alignment from sequence metadata.
+#
+use strict;
+use warnings;
+use Getopt::Long qw(:config no_auto_abbrev);
+use Time::HiRes qw(gettimeofday);
+use Bio::Easel::MSA;
+use Bio::Easel::SqFile;
+
+require "vadr.pm";
+require "sqp_opts.pm";
+require "sqp_ofile.pm";
+require "sqp_seq.pm";
+require "sqp_seqfile.pm";
+require "sqp_utils.pm";
+
+#######################################################################################
+# What this script does: 
+#
+# - Preliminaries: 
+#   o processes options
+#   o creates the output directory
+#   o outputs program banner
+#   o makes sure the required executables are executable
+#
+# - Reads metadata output from fetch-seqs-given-taxid.pl
+# - Selects sequence representatives (The Funnel Strategy)
+# - Downloads full fasta files
+# - Assesses quality and filters based on length/ambiguities
+# - Computes alignment and constructs final multi-seq Stockholm + minfo
+#######################################################################################
+
+# make sure required environment variables are set
+my $env_vadr_scripts_dir  = utl_DirEnvVarValid("VADRSCRIPTSDIR");
+my $env_vadr_blast_dir    = utl_DirEnvVarValid("VADRBLASTDIR");
+my $env_vadr_infernal_dir = utl_DirEnvVarValid("VADRINFERNALDIR");
+my $env_vadr_hmmer_dir    = utl_DirEnvVarValid("VADRHMMERDIR");
+my $env_vadr_easel_dir    = utl_DirEnvVarValid("VADREASELDIR");
+
+# make sure the required executables exist and are executable
+my %execs_H = (); # hash with paths to all required executables
+$execs_H{"esl-reformat"}  = $env_vadr_easel_dir    . "/esl-reformat";
+$execs_H{"esl-sfetch"}    = $env_vadr_easel_dir    . "/esl-sfetch";
+utl_ExecHValidate(\%execs_H, undef);
+
+#########################################################
+# Command line and option processing using sqp_opts.pm
+#
+my %opt_HH = ();      
+my @opt_order_A = (); 
+my %opt_group_desc_H = ();
+my $g = 0; # option group
+
+$opt_group_desc_H{++$g} = "basic options";
+#     option            type       default  group   requires incompat     preamble-output                                                help-output            
+opt_Add("-h",           "boolean", 0,           0,    undef, undef,       undef,                                                         "display this help",                                   \%opt_HH, \@opt_order_A);
+opt_Add("-f",           "boolean", 0,          $g,    undef, undef,       "forcing directory overwrite",                                 "force; if dir <output directory> exists, overwrite it", \%opt_HH, \@opt_order_A);
+opt_Add("-v",           "boolean", 0,          $g,    undef, undef,       "be verbose",                                                  "be verbose; output commands to stdout as they're run", \%opt_HH, \@opt_order_A);
+opt_Add("--keep",       "boolean", 0,          $g,    undef, undef,       "leave intermediate files on disk",                            "do not remove intermediate files, keep them all on disk", \%opt_HH, \@opt_order_A);
+
+$opt_group_desc_H{++$g} = "input options";
+#     option            type       default  group   requires incompat     preamble-output                                                help-output            
+opt_Add("--minout",     "string",  undef,      $g,    undef, undef,       "output model info file to <s>",                               "output model info file to <s>", \%opt_HH, \@opt_order_A);
+
+$opt_group_desc_H{++$g} = "other expert options";
+#       option       type          default     group  requires incompat      preamble-output                                              help-output           
+opt_Add("--execname",   "string",  undef,         $g,    undef, undef,       "define executable name of this script as <s>",              "define executable name of this script as <s>", \%opt_HH, \@opt_order_A);
+
+my %GetOptions_H = ();
+my $options_okay = 
+    &GetOptions('h'            => \$GetOptions_H{"-h"}, 
+# basic options
+                'f'            => \$GetOptions_H{"-f"},
+                'v'            => \$GetOptions_H{"-v"},
+                'keep'         => \$GetOptions_H{"--keep"},
+# input options
+                'minout=s'     => \$GetOptions_H{"--minout"},
+# other expert options
+                'execname=s'   => \$GetOptions_H{"--execname"});
+
+my $total_seconds = -1 * ofile_SecondsSinceEpoch(); 
+my $execname_opt  = $GetOptions_H{"--execname"};
+my $executable    = (defined $execname_opt) ? $execname_opt : "v-profprep.pl";
+my $usage         = "Usage: $executable [-options] <sequence metadata tsv> <path to output directory to create>\n";
+my $synopsis      = "$executable :: prepare training alignment for profile model building";
+my $date          = scalar localtime();
+my $version       = "1.7";
+my $releasedate   = "Sep 2025";
+my $pkgname       = "VADR";
+
+# print help and exit if necessary
+if((! $options_okay) || ($GetOptions_H{"-h"})) { 
+  ofile_OutputBanner(*STDOUT, $pkgname, $version, $releasedate, $synopsis, $date, undef);
+  opt_OutputHelp(*STDOUT, $usage, \%opt_HH, \@opt_order_A, \%opt_group_desc_H);
+  if(! $options_okay) { die "ERROR, unrecognized option;"; }
+  else                { exit 0; } 
+}
+
+# check that number of command line args is correct
+if(scalar(@ARGV) != 2) {   
+  print "Incorrect number of command line arguments.\n";
+  print $usage;
+  print "\nTo see more help on available options, do $executable -h\n\n";
+  exit(1);
+}
+my ($meta_tsv, $dir) = (@ARGV);
+
+# output banner
+my $opt_str    = opt_GetString(*STDOUT, \%opt_HH, \@opt_order_A);
+my $output_dir = ofile_CreateAndOpenDirectory($dir, $GetOptions_H{"-f"}, $opt_str, $executable, undef, $GetOptions_H{"-v"});
+my $dir_tail   = utl_DirTail($dir);
+
+# Check input files
+utl_FileDieIfNotExist($meta_tsv, "metadata TSV file");
+
+ofile_OutputString(*STDOUT, 1, sprintf("# %-32s %s\n", "metadata TSV file:", $meta_tsv));
+ofile_OutputString(*STDOUT, 1, sprintf("# %-32s %s\n", "output directory:",  $dir));
+
+my $log_FH  = ofile_FileHandle("$dir/$dir_tail.vadr.log");
+ofile_OutputBanner($log_FH, $pkgname, $version, $releasedate, $synopsis, $date, $dir);
+ofile_OutputString($log_FH, 1, $opt_str);
+
+#---------------------------------------
+# Step 1: Read and filter metadata
+#---------------------------------------
+
+# TODO: We need the seed model length to filter >90%. For now, assume a placeholder or add a command-line arg later.
+my $seed_model_len = 0; # Placeholder
+
+# %candidate_AH: arrays of hashes [1..nseq-1] grouped by serotype/genotype keys.
+# Each hash contains sequence metadata:
+#   acc      => sequence accession
+#   len      => sequence length
+#   cdate    => genbank create date
+#   serotype => sequence serotype
+#   genotype => sequence genotype
+#   isolate  => sequence isolate
+my %candidate_AH = ();
+parse_and_filter_metadata($meta_tsv, $seed_model_len, \%candidate_AH);
+
+ofile_OutputString(\*STDOUT, 1, sprintf("All done.\n"));
+
+$total_seconds += ofile_SecondsSinceEpoch();
+utl_OutputTotalTime(\*STDOUT, $total_seconds, 1, 1);
+exit(0);
+
+
+#################################################################
+# Subroutine : parse_and_filter_metadata()
+# Incept     : Gemini Thu Mar 05 2026
+#
+# Purpose    : Reads the sequence metadata TSV, applies length
+#              filters, and performs the Chronological Sampling
+#              Tier 1 filter to select the diverse 50 per group.
+#
+# Arguments  :
+#   $tsv_file       : path to metadata TSV file
+#   $seed_model_len : length of the seed model for >90% filtering
+#   $candidate_AHR  : reference to hash mapping group (serotype/genotype)
+#                     to an array ref of selected sequence hashrefs.
+#
+# Returns    : void
+#################################################################
+sub parse_and_filter_metadata {
+  my ($tsv_file, $seed_model_len, $candidate_AHR) = @_;
+
+  my $total_seqs = 0;
+  my $kept_len_seqs = 0;
+
+  open(my $tsv_fh, "<", $tsv_file) or die "ERROR: Cannot read $tsv_file: $!";
+  my $header = <$tsv_fh>; # Skip header: Accession, Length, CreateDate, Serotype, Genotype, Isolate
+
+  while (my $line = <$tsv_fh>) {
+    chomp $line;
+    my ($acc, $len, $cdate, $serotype, $genotype, $isolate) = split(/\t/, $line);
+    $total_seqs++;
+    
+    # Length check: Keep sequences >90% of the seed model's length
+    if ($seed_model_len > 0 && $len < ($seed_model_len * 0.90)) {
+      next;
+    }
+    $kept_len_seqs++;
+
+    # Determine grouping key
+    my $group = "Unknown";
+    if    (defined $serotype && $serotype ne "") { $group = $serotype; }
+    elsif (defined $genotype && $genotype ne "") { $group = $genotype; }
+
+    # Save candidate info
+    push @{$candidate_AHR->{$group}}, {
+      acc      => $acc,
+      len      => $len,
+      cdate    => $cdate,
+      serotype => $serotype,
+      genotype => $genotype,
+      isolate  => $isolate
+    };
+  }
+  close($tsv_fh);
+
+  ofile_OutputString(\*STDOUT, 1, sprintf("# Parsed %d total sequences from TSV.\n", $total_seqs));
+  if ($seed_model_len > 0) {
+    ofile_OutputString(\*STDOUT, 1, sprintf("# Retained %d sequences passing length filter (>90%% of seed length %d).\n", $kept_len_seqs, $seed_model_len));
+  }
+  ofile_OutputString(\*STDOUT, 1, sprintf("# Found %d distinct serotype/genotype groups.\n", scalar(keys %{$candidate_AHR})));
+
+  #---------------------------------------
+  # Tier 1 Filter: Chronological Sampling
+  #---------------------------------------
+  my $max_per_group = 50;
+  my $total_selected = 0;
+
+  foreach my $group (sort keys %{$candidate_AHR}) {
+    my @seqs = @{$candidate_AHR->{$group}};
+
+    # Parse CreateDate and prepare for sorting
+    foreach my $seq (@seqs) {
+      my $cdate = $seq->{cdate} || "";
+      $seq->{year_month} = "0000-00";
+      $seq->{year_month_day} = "0000-00-00";
+      $seq->{date_val} = "9999/99/99"; # default sort to end if invalid
+
+      if ($cdate =~ m/^(\d{4})\/(\d{2})\/(\d{2})/) {
+         $seq->{year_month}     = "$1-$2";
+         $seq->{year_month_day} = "$1-$2-$3";
+         $seq->{date_val}       = $cdate;
+      }
+    }
+
+    # Sort oldest to newest (alphabetical on YYYY/MM/DD works nicely here)
+    @seqs = sort { $a->{date_val} cmp $b->{date_val} || $a->{acc} cmp $b->{acc} } @seqs;
+
+    my @selected_for_group = ();
+    my %seen_month = ();
+    my %seen_day =();
+    my %is_selected = ();
+
+    # Pass 1: 1 per year/month
+    foreach my $seq (@seqs) {
+      last if scalar(@selected_for_group) >= $max_per_group;
+      if (! $seen_month{$seq->{year_month}}) {
+        push @selected_for_group, $seq;
+        $seen_month{$seq->{year_month}} = 1;
+        $seen_day{$seq->{year_month_day}} = 1; # Reserve the day so pass 2 skips it
+        $is_selected{$seq->{acc}} = 1;
+      }
+    }
+
+    # Pass 2: 1 per day
+    if (scalar(@selected_for_group) < $max_per_group) {
+      foreach my $seq (@seqs) {
+        last if scalar(@selected_for_group) >= $max_per_group;
+        next if $is_selected{$seq->{acc}};
+        if (! $seen_day{$seq->{year_month_day}}) {
+          push @selected_for_group, $seq;
+          $seen_day{$seq->{year_month_day}} = 1;
+          $is_selected{$seq->{acc}} = 1;
+        }
+      }
+    }
+
+    # Pass 3: Oldest remaining
+    if (scalar(@selected_for_group) < $max_per_group) {
+      foreach my $seq (@seqs) {
+        last if scalar(@selected_for_group) >= $max_per_group;
+        next if $is_selected{$seq->{acc}};
+        push @selected_for_group, $seq;
+        $is_selected{$seq->{acc}} = 1;
+      }
+    }
+
+    # Replace the original group array with the filtered top 50
+    $candidate_AHR->{$group} = \@selected_for_group;
+    $total_selected += scalar(@selected_for_group);
+  }
+
+  ofile_OutputString(\*STDOUT, 1, sprintf("# Tier 1 Filter: Selected %d total sequences (max %d per group) for tier 2 processing.\n", $total_selected, $max_per_group));
+
+  return;
+}
+
